a75f404c629392f9b8caa810a547286982beeff5
s10boi <avonpup@gmail.com>
2025-07-08 14:44:18 +0100

    Update pedalboard dependency version and remove macOS overrides

--- src/audio/speech/speech.py
+++ src/audio/speech/speech.py
@@ -1,16 +1,10 @@
-import sys
+from typing import cast
 
 import pyttsx3
 
 
 def get_engine() -> pyttsx3.Engine:
-    """Overrides default pyttsx3 engine to fix issues with macOS TTS."""
-    if sys.platform == "darwin":
-        from src.audio.speech.macos_override.engine import get_hypno_engine  # type: ignore  # noqa: PLC0415
-
-        engine = get_hypno_engine()
-    else:
-        engine = pyttsx3.init()  # type: ignore
+    engine = cast("pyttsx3.Engine", pyttsx3.init())
 
     engine.setProperty("rate", 110)
     engine.setProperty("volume", 0.8)
